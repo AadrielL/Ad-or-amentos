f03378a2f5e153f1f85d2b7beed8d02d2d7cf7b0
Adriel <Adrie3lmorais@gmail.com>
2025-11-06 18:30:21 -0300

    Estrutura do Backend e Entidades Iniciais (Usuario, CodigoAdmin, Material, ServicoPadrao)

---
+++ src/main/java/com/adcomandos/AdComandosApplication.java
@@ -0,0 +1,13 @@
+package com.adcomandos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdComandosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdComandosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adcomandos/AdComandosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adcomandos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdComandosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
